7c24234988fc736b6d1d80a53bef525a059cc032
nicolas.dominutti@rockingdata.co <nicolas.dominutti@rockingdata.co>
2023-05-06 10:41:40 -0300

    develop writing to RDS function

--- dags/src/dagsUtils.py
+++ dags/src/dagsUtils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from . import S3utils
+import psycopg2
 
 
 def filter_data(bucket_name, raw_data_file_path, act_adv_file_path, output_file_path):
@@ -14,3 +15,34 @@ def train_job(model, bucket_name, curated_data_file_path, output_file_path):
     model_instance = model(curated_data)
     recommendation = model_instance.top_20()
     S3utils.post_data(bucket_name, output_file_path, recommendation)
+
+
+def write_rds(bucket_name, recommendation_file_path, model_type):
+    recommendation = S3utils.get_data(bucket_name, recommendation_file_path)
+
+    engine = psycopg2.connect(
+        database="postgres",
+        host="database-pa-udesa-test.c6z3l3m7uu0r.us-east-2.rds.amazonaws.com",
+        port=5432,
+    )
+
+    cursor = engine.cursor()
+    if model_type == "products":
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS LATEST_PRODUCT_RECOMMENDATION (ADVERTISER VARCHAR(50),
+                                                                                    PRODUCT VARCHAR(50),
+                                                                                    DATE TIMESTAMP,
+                                                                                    PRIMARY KEY (ADVERTISER, PRODUCT));"""
+        )
+        cursor.execute("""TRUNCATE TABLE LATEST_PRODUCT_RECOMMENDATION;""")
+        for index, row in recommendation.iterrows():
+            cursor.execute(
+                """INSERT INTO table_name (ADVERTISER, PRODUCT, DATE) 
+                                                    VALUES (%s, %s, %s)""",
+                (row["ADVERTISER"], row["PRODUCT"], row["DATE"]),
+            )
+
+    engine.commit()
+    cursor.close()
+    engine.close()
+    print("Insert Success")
