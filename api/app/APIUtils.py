95c1ddd4d9448092f6a3d6ab306e0726b2d4b1ab
nicolas.dominutti@rockingdata.co <nicolas.dominutti@rockingdata.co>
2023-05-08 19:57:47 -0300

    minor fix con returnable dataframe filtering

--- api/app/APIUtils.py
+++ api/app/APIUtils.py
@@ -2,10 +2,16 @@ import pandas as pd
 
 
 def query_latest_recommendation(advertiser_id, model_type, engine):
+    if not model_type in ["products", "ctr"]:
+        return {"Error": 'El model_type debe ser "products" o "ctr"'}
     table_names = {
         "products": "LATEST_PRODUCT_RECOMMENDATION",
         "ctr": "LATEST_ADVERTISERS_RECOMMENDATION",
     }
     query = f"""SELECT * FROM {table_names[model_type]} WHERE ADVERTISER = '{advertiser_id}'"""
     dataframe = pd.read_sql(query, engine)
-    return_data = {advertiser_id: dataframe["product"].values.tolist()}
+    if dataframe.shape[0] < 0:
+        return {
+            "Error": "El advertiser ingresado no es válido o no existe en la base de datos"
+        }
+    return {advertiser_id: dataframe["product"].values.tolist()}
