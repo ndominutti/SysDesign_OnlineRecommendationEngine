50973c58fcb7f5a035ceeb8b6d4e350a8c854f35
nicolas.dominutti@rockingdata.co <nicolas.dominutti@rockingdata.co>
2023-05-08 19:20:23 -0300

    minor fix in query to RDS

--- api/app/APIUtils.py
+++ api/app/APIUtils.py
@@ -6,6 +6,6 @@ def query_latest_recommendation(advertiser_id, model_type, engine):
         "products": "LATEST_PRODUCT_RECOMMENDATION",
         "ctr": "LATEST_ADVERTISERS_RECOMMENDATION",
     }
-    query = f"""SELECT * FROM {table_names[model_type]} WHERE "ADVERTISER"={advertiser_id}"""
+    query = f"""SELECT * FROM {table_names[model_type]} WHERE ADVERTISER = '{advertiser_id}'"""
     dataframe = pd.read_sql(query, engine)
-    return_data = {advertiser_id: dataframe.PRODUCT.values.tolist()}
+    return_data = {advertiser_id: dataframe.product.values.tolist()}
