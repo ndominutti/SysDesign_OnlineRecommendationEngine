b8ad51b83897b003bb3dd7bafaec5a9e54757bf0
nicolas.dominutti@rockingdata.co <nicolas.dominutti@rockingdata.co>
2023-05-20 13:22:41 -0300

    test crone different hour

--- dags/dag.py
+++ dags/dag.py
@@ -29,7 +29,7 @@ from src.topctr import Topctr
 
 with DAG(
     dag_id="ad_recommender",
-    schedule_interval="10 16 * * *",
+    schedule_interval="35 16 * * *",
     start_date=datetime(2023, 5, 10),
     catchup=False,
 ) as dag:
